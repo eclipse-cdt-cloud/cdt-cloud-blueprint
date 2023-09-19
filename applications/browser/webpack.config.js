ad181773a48ac793b15656734ffa7037ec0acfba
Simon Graband <sgraband@eclipsesource.com>
2023-09-19 14:17:59 +0200

    Bundle examples with webpack
    This makes sure that they are available during runtime.
    Use _dirname instead of module.path as the latter doesn't work.
    Delete generated *.gz files after webpack build.
    Also bundle examples with the electron-builder for the AppImage.
    
    Contributed on behalf of STMicroelectronics

--- applications/browser/webpack.config.js
+++ applications/browser/webpack.config.js
@@ -5,6 +5,10 @@
 // @ts-check
 const config = require('./gen-webpack.config.js');
 const backend = require('./gen-webpack.node.config.js');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const RemovePlugin = require('remove-files-webpack-plugin')
+const path = require('path');
+const resolvePackagePath = require('resolve-package-path');
 
 /**
  * Expose bundled modules on window.theia.moduleName namespace, e.g.
@@ -19,6 +23,34 @@ if (process.platform !== 'win32') {
     // For some reason, blueprint wants to bundle the `.node` files directly without going through `@vscode/windows-ca-certs`
     backend.ignoredResources.add('@vscode/windows-ca-certs/build/Release/crypt32.node');
 }
+
+// Copy example resources
+const plugins = [
+    new CopyWebpackPlugin({
+        patterns: [
+            {
+                // copy examples to resource folder
+                from: path.join(resolvePackagePath('@eclipse-cdt-cloud/blueprint-examples', __dirname), '..', 'resources'),
+                to: path.resolve(__dirname, 'resources')
+            }
+        ]
+    }),
+    new RemovePlugin({
+        after: {
+            test: [
+                {
+                    folder: path.resolve(__dirname, 'resources'),
+                    method: (absoluteItemPath) => {
+                        return new RegExp(/^.*\.gz$/).test(absoluteItemPath);
+                    },
+                    recursive: true
+                }
+            ]
+        }
+    })
+]
+
+config[0].plugins.push(...plugins);
 
 module.exports = [
     ...config,
